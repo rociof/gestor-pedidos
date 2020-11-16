d3a95eb43d0dd1dad746d9a9df83531cf8a05857
rociof <rociofernandezcanto@gmail.com>
2020-11-16 19:31:27 +0100

    proveedor

--- routes/proveedor.routes.js
+++ routes/proveedor.routes.js
@@ -4,11 +4,16 @@ const router = express.Router();
 const Proveedor = require("../models/proveedor");
 // const User = require('../models/User');
 
-// READ -- Leo todos los datos
-router.get("/", (req, res) => {
+//READ -- Leo todos los datos
+router.get('/', async function(req, res, next) {
+  let proveedor = await Proveedor.findAll();
+  res.render("proveedor", {proveedor, session:req.session});
+});
+
+
+router.get("/nuevo", (req, res) => {
   Proveedor.findAll().then((proveedor) => {
-    res.json(proveedor);
-    //res.render('clientes', { clientes });
+    res.render("frmRegistroProveedor", {proveedor});
     
   });
 });
@@ -21,31 +26,9 @@ router.get("/:id", (req, res) => {
 });
 
 // Ingreso Datos
-router.post("/", (req, res) => {
+router.post("/nuevo", (req, res) => {
   Proveedor.create({
-    DNI: req.body.DNI,
-    Nombre: req.body.Nombre,
-    Apellido: req.body.Apellido,
-    Email: req.body.Email,
-    Direccion: req.body.Direccion,
-    Localidad: req.body.Localidad,
-    CP: req.body.CP,
-    Provincia: req.body.Provincia,
-    Telefono: req.body.Telefono,
-   
-  })
-    .then((proveedor) => {
-      res.json(proveedor);
-    })
-    .catch((err) => {
-      res.json(err);
-    });
-});
-
-// UPDATE - Actualizo datos
-router.put("/:id", (req, res) => {
-  Proveedor.update(
-    {
+      DNI: req.body.DNI,
       Nombre: req.body.Nombre,
       Apellido: req.body.Apellido,
       Email: req.body.Email,
@@ -55,30 +38,52 @@ router.put("/:id", (req, res) => {
       Provincia: req.body.Provincia,
       Telefono: req.body.Telefono,
       
-    },
-    {
+    })
+    .then((proveedor) => {
+    res.redirect("/");
+      
+    })
+    .catch((err) => {
+      res.json(err);
+    });
+  
+});
+
+// UPDATE - Actualizo datos
+router.put("/:id", (req, res) => {
+  Proveedor.update({
+      Nombre: req.body.Nombre,
+      Apellido: req.body.Apellido,
+      Email: req.body.Email,
+      Direccion: req.body.Direccion,
+      Localidad: req.body.Localidad,
+      CP: req.body.CP,
+      Provincia: req.body.Provincia,
+      Telefono: req.body.Telefono,
+      
+    }, {
       where: {
         DNI: req.params.id,
       },
-    }
-  ).then((resultado) => {
-    res.json(resultado);
-  })
-  .catch((err) => {
-    res.json(err);
-  });
-});
-
-
-// DELETE un Proveedor
-router.delete('/:id',(req,res)=>{
-    Proveedor.destroy({
-        where: {
-            DNI: req.params.id
-        }
-    }).then(resultado => {
-        res.json(resultado);
+    })
+    .then((resultado) => {
+      res.json(resultado);
+    })
+    .catch((err) => {
+      res.json(err);
     });
 });
 
+// DELETE un cliente
+router.delete("/:id", (req, res) => {
+  Proveedor.destroy({
+    where: {
+      DNI: req.params.id,
+    },
+  }).then((resultado) => {
+    res.json(resultado);
+  });
+});
+
 module.exports = router;
+
